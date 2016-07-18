a01cc810b5252c57644494feb552e350ae8f3890
Ari Porad <ari@ariporad.com>
2016-07-17 20:48:42 -0700

    fix: on really large worlds, the overlay boxes are still readable

--- src/KarelWorld/Overlay.jsx
+++ src/KarelWorld/Overlay.jsx
@@ -1,5 +1,5 @@
-const BOX_HEIGHT = 150;
-const BOX_WIDTH = 300;
+const BOX_HEIGHT_FACTOR = 0.375;
+const BOX_WIDTH_FACTOR = 0.75;
 
 const styles = {
   text: {
@@ -12,27 +12,31 @@ const styles = {
 };
 
 const Overlay = Radium(({ fill, title, height, width, children }) => {
-  let titleY = React.Children.count(children) > 0 ? height / 2 - 10 : height / 2;
+  let titleY = React.Children.count(children) > 0 ? height / 2 - (height * (1/15)) : height / 2;
+  const boxWidth = BOX_WIDTH_FACTOR * width;
+  const boxHeight = BOX_HEIGHT_FACTOR * height;
   return (
     <g>
       <rect x={0} y={0} width={width} height={height} fill="#888888" opacity={0.9} />
       <rect
-        x={(width - BOX_WIDTH) / 2}
-        y={(height - BOX_HEIGHT) / 2}
-        rx={20}
-        ry={20}
-        width={BOX_WIDTH}
-        height={BOX_HEIGHT}
+        x={(width - boxWidth) / 2}
+        y={(height - boxHeight) / 2}
+        rx={width/20}
+        ry={width/20}
+        width={boxWidth}
+        height={boxHeight}
         fill={fill}
       />
       <text
         textAnchor="middle"
         x={width / 2}
         y={titleY}
-        style={[styles.text, styles.title]}
+        style={[styles.text, styles.title, { fontSize: width / 17 }]}
         fill="#FFFFFF"
       >{title}</text>
-      {children}
+      <g style={[{ fontSize: width / 25 }]}>
+        {children}
+      </g>
     </g>
   );
 });
